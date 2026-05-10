b3df1bf96005f5cad232ac5c382e42c44632f3d8
Maaz Ahmed <76902894+maazahmed408@users.noreply.github.com>
2026-05-10 11:01:54 -0400

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+       int l = 0;
+       int maxProfit = 0;
+        
+        for(int r = 1; r<prices.size(); ++r){
+            maxProfit = max(maxProfit,prices[r] - prices[l]);
+            if(prices[r] < prices[l]){
+                l = r;
+            }
+        }
+
+        return maxProfit;
+    }
+};
