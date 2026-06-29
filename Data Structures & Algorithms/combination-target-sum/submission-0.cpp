d557fcafb0da60333aea9cd41fa770beeafe0feb
Maaz Ahmed <76902894+maazahmed408@users.noreply.github.com>
2026-06-29 06:13:48 -0400

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        dfs(0,{},0,nums,target);
+        return res;
+    }
+private:
+    void dfs(int i , vector<int> curr, int total,vector<int>& nums,int target){
+        if (total == target){
+            auto copy = curr;
+            res.push_back(copy);
+            return;
+        }
+        if(i >= nums.size() || total > target){
+            return ;
+        }
+
+        curr.push_back(nums[i]);
+        dfs(i,curr,total+nums[i],nums,target);
+
+        curr.pop_back();
+        dfs(i+1,curr,total,nums,target);
+    }
+};
