3684688d597eeed95132dda5a9e400ba2dd2e001
Maaz Ahmed <76902894+maazahmed408@users.noreply.github.com>
2026-06-30 17:58:09 -0400

    Add: longest-repeating-substring-with-replacement - submission-12

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-12.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> hashmap;
+        int maxFreq = 0;
+        int maxLen = 0;
+        int count = 0;
+        int l = 0;
+        for(int r = 0 ; r < s.size(); ++r){
+            hashmap[s[r]]++;
+            maxFreq = max(maxFreq,hashmap[s[r]]);
+
+            while((r-l+1) - maxFreq  > k){
+            hashmap[s[l]]--;
+            l++;
+           }
+
+           maxLen = max(maxLen, r-l+1);
+        }
+
+        return maxLen;
+    }
+
+};
