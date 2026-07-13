0e5e13790595bdaaca0b5d117b2f2793ece92f51
Maaz Ahmed <76902894+maazahmed408@users.noreply.github.com>
2026-07-13 19:54:39 -0400

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    unordered_map<Node*,Node*> oldToNew;
+public:
+    Node* cloneGraph(Node* node) {
+
+        if(node == nullptr) return nullptr;
+
+        if(oldToNew.count(node)) return oldToNew[node];
+
+        Node* copyNode = new Node(node->val);
+
+        oldToNew[node] = copyNode;
+
+        for(Node* nei : node->neighbors){
+            copyNode->neighbors.push_back(cloneGraph(nei));
+        }
+
+        return copyNode;
+
+    }
+};
