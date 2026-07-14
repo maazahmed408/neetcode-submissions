1ac5b5ba38c6c17aea096f16333459a5c02adc9c
Maaz Ahmed <76902894+maazahmed408@users.noreply.github.com>
2026-07-13 23:14:53 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    unordered_map<int,vector<int>> adj;
+    unordered_set<int> visited;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        for(int i = 0 ; i < numCourses; ++i){
+            adj[i] = {};
+        }
+
+        for(auto prereq : prerequisites){
+            int n1 = prereq[0];
+            int n2 = prereq[1];
+            adj[n1].push_back(n2);
+        }
+
+        for(int i = 0 ; i < numCourses ; ++i){
+            if(!dfs(i)) return false;
+        }
+
+        return true;
+    }
+
+    bool dfs(int i){
+        if(visited.count(i)) return false;
+
+        if(adj[i].empty()) return true;
+
+        visited.insert(i);
+
+        for(int n : adj[i]){
+            if(!dfs(n)) return false;
+        }
+        visited.erase(i);
+
+        return true;
+    }
+};
